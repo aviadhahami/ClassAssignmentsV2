52e565f8a5ae96b2e06f70dce29f4049e21d1e55
Aviad Hahami <aviadhahami@gmail.com>
2015-03-20 00:37:17 +0200

    fixed with of input boxes for the date

--- app/scripts/controllers/addtask.js
+++ app/scripts/controllers/addtask.js
@@ -8,33 +8,32 @@
  * Controller of the classAssignmentsV2App
  */
 angular.module('classAssignmentsV2App')
-  .controller('AddtaskCtrl', function ($scope,setClassContentFactory) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-     $scope.user = {
-      subject: 'Write Task Subject Here',
-      due:{
-          year:'',
-          month:'',
-          day:''
-      },
-      number:'number here'
-    };
-    $scope.alert = function (){
-        console.log(1);
-    };
-    $scope.createTask = function(){
-    	setClassContentFactory.setContent($scope.user);
-        console.log($scope.user);
-    };
-  })
-  .config( function($mdThemingProvider){
-    // Configure a dark theme with primary foreground yellow
-    $mdThemingProvider.theme('docs-dark', 'default')
-        .primaryPalette('yellow')
-        .dark();
-  });
- 
+        .controller('AddtaskCtrl', function ($scope, setClassContentFactory) {
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+            var d = new Date();
+            $scope.user = {
+                subject: 'Write Task Subject Here',
+                due: {
+                    year: d.getFullYear(),
+                    month: d.getMonth() + 1,
+                    day: d.getDate()
+                },
+                number: 'number here'
+            };
+
+            $scope.createTask = function () {
+                setClassContentFactory.setContent($scope.user);
+                console.log($scope.user);
+            };
+        })
+        .config(function ($mdThemingProvider) {
+            // Configure a dark theme with primary foreground yellow
+            $mdThemingProvider.theme('docs-dark', 'default')
+                    .primaryPalette('yellow')
+                    .dark();
+        });
+

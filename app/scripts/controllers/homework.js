c03ddffc45aa4b6372d0bd6cebef770afcb59149
Aviad Hahami <aviadhahami@gmail.com>
2015-03-26 17:39:54 +0200

    implementing new Date logic
    will show days to date

--- app/scripts/controllers/homework.js
+++ app/scripts/controllers/homework.js
@@ -7,27 +7,25 @@
  * # HomeworkCtrl
  * Controller of the classAssignmentsV2App
  */
-angular.module('classAssignmentsV2App')
-        .controller('HomeworkCtrl', function ($scope,getClassContentFactory) {
-
-            $scope.awesomeThings = [
-                'HTML5 Boilerplate',
-                'AngularJS',
-                'Karma'
-            ];
+ angular.module('classAssignmentsV2App')
+ .controller('HomeworkCtrl', function ($scope,getClassContentFactory) {
 
             //=======AJAX REQUEST ! =========
             getClassContentFactory.getContent('data/idc/', 'cs16')
-                    .success(function (data) {
-                        $scope.assignList = data;
+            .success(function (data) {
+                $scope.assignList = data;
                         //converting the JSON to Date() object
+                        //Get 1 day in milliseconds
+                        var oneDay=1000*60*60*24;
+                        var today = new Date();
                         angular.forEach(data, function (item) {
                             item.due = new Date(item.due.year, item.due.month, item.due.day);
+                            item.timeLeft = Math.round((item.due-today)/oneDay);
                         });
                     }).error(function (data) {
-                console.log('CRAZY ERROR!');
-                console.log(data);
-            });
+                        console.log('CRAZY ERROR!');
+                        console.log(data);
+                    });
 //            var json = {'records': [{'number': '1', 'subject': 'Operating Systems', 'due': {'year': 2015, 'month': 3, 'day': 22}}, {'number': '1', 'subject': 'Digital Graphics', 'due': {'year': 2015, 'month': 3, 'day': 22}}, {'number': '2', 'subject': 'Funding', 'due': {'year': 2015, 'month': 3, 'day': 16}}, {'number': '1', 'subject': 'Statistics', 'due': {'year': 2015, 'month': 3, 'day': 16}}, {'number': '1', 'subject': 'testtt', 'due': {'year': 2015, 'month': 3, 'day': 22}}]};
 //            $scope.assignList = json;
 //            angular.forEach(json, function (item) {
@@ -36,4 +34,4 @@ angular.module('classAssignmentsV2App')
 
 
 
-        });
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,28 @@
  * # MainCtrl
  * Controller of the classAssignmentsV2App
  */
-angular.module('classAssignmentsV2App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('classAssignmentsV2App')
+ .controller('MainCtrl', function ($scope,getClassContentFactory) {
+ 	$scope.calcDays = function (date1,date2){
+ 		var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+ 		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+ 		return diffDays;
+ 	};
+  //=======AJAX REQUEST ! =========
+  getClassContentFactory.getContent('data/idc/', 'cs16')
+  .success(function (data) {
+  	$scope.assignList = data;
+                        //converting the JSON to Date() object
+                        //Get 1 day in milliseconds
+                        var today = new Date();
+                        angular.forEach(data, function (item) {
+                        	var dateStr = item.due.month + '/' + item.due.day + '/' + item.due.year;
+                        	item.due = new Date(dateStr);
+                        	//item.due = new Date(item.due.year, item.due.month, item.due.day);
+                        	item.timeLeft = $scope.calcDays(today,item.due);
+                        });
+                    }).error(function (data) {
+                    	console.log('CRAZY ERROR!');
+                    	console.log(data);
+                    });
+                });

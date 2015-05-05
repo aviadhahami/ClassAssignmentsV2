3ab68bbf286b68cbbbbf5d686e4fc083c4eeb7eb
Aviad Hahami <aviadhahami@gmail.com>
2015-05-05 17:03:17 +0300

    Revert "Added stupid hourly check"
    This reverts commit 00b8524ffa82a04dfadeba9db2b851c424a364c1.

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -1,61 +1,54 @@
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 'use strict';
 angular.module('customFilters', []).filter('futureDates', function () {
-  return function (items) {
-    var filtered = [];
-    var date = new Date();
-    angular.forEach(items, function (item) {
-      if (item) {
-        if (item.due.getFullYear() > date.getFullYear()) {
-          filtered.push(item);
-        } else if (item.due.getFullYear() === date.getFullYear()) { //year is even
-          if (item.due.getMonth() > date.getMonth() + 1) {
-            filtered.push(item);
-          } else if (item.due.getMonth() === date.getMonth() + 1) { //month even
-            if (item.due.getDate() > date.getDate()) {
-              filtered.push(item);
-            }else if (item.due.getDate() === date.getDate()) {
-              if (item.due.getHours() > date.getHours()) {
-                filtered.push(item);
-              }else if (item.due.getHours() === date.getHours()) {
-                if (item.due.getMinutes() >=  date.getMinutes()) {
-                  filtered.push(item);
+    return function (items) {
+        var filtered = [];
+        var date = new Date();
+        angular.forEach(items, function (item) {
+            if (item) {
+                if (item.due.getFullYear() > date.getFullYear()) {
+                    filtered.push(item);
+                } else if (item.due.getFullYear() === date.getFullYear()) { //year is even
+                    if (item.due.getMonth() > date.getMonth() + 1) {
+                        filtered.push(item);
+                    } else if (item.due.getMonth() === date.getMonth() + 1) { //month even
+                        if (item.due.getDate() >= date.getDate()) { 
+                            //TODO: implemenet hour check
+                            filtered.push(item);
+                        }
+                    }
                 }
-              }
+            } else {
+                console.log('dead item');
             }
-          }
-        }
-      } else {
-        console.log('dead item');
-      }
-    });
-    return filtered;
-  };
+        });
+        return filtered;
+    };
 }).filter('pastDate', function () {
-  return function (items) {
-    var filtered = [];
-    var date = new Date();
-    angular.forEach(items, function (item) {
-      if (item) {
-        if (item.due.getFullYear() < date.getFullYear()) {
-          filtered.push(item);
-        } else if (item.due.getFullYear() === date.getFullYear()) { //year is even
-          if (item.due.getMonth() < date.getMonth() + 1) {
-            filtered.push(item);
-          } else if (item.due.getMonth() === date.getMonth() + 1) { //month even
-            if (item.due.getDate() < date.getDate()) {
-              filtered.push(item);
+    return function (items) {
+        var filtered = [];
+        var date = new Date();
+        angular.forEach(items, function (item) {
+            if (item) {
+                if (item.due.getFullYear() < date.getFullYear()) {
+                    filtered.push(item);
+                } else if (item.due.getFullYear() === date.getFullYear()) { //year is even
+                    if (item.due.getMonth() < date.getMonth() + 1) {
+                        filtered.push(item);
+                    } else if (item.due.getMonth() === date.getMonth() + 1) { //month even
+                        if (item.due.getDate() < date.getDate()) {
+                            filtered.push(item);
+                        }
+                    }
+                }
+            } else {
+                console.log('dead item');
             }
-          }
-        }
-      } else {
-        console.log('dead item');
-      }
-    });
-    return filtered;
-  };
+        });
+        return filtered;
+    };
 });
